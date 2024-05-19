dd7b175a8e62d11ed1c5b8f811a84e56dde4d575
Shariful4447 <shariful.islam4447@gmail.com>
2024-05-20 00:37:16 +0200

    get post done

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -17,6 +17,20 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+export const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Students are retrived successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
 };
